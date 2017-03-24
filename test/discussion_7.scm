a1ffd56260aad0b057f39da8615343ee05f26925
Arjun Nair <a26nair@edu.uwaterloo.ca>
2017-03-23 22:14:22 -0400

    fixed formatting in discussion_7

--- test/discussion_7.scm
+++ test/discussion_7.scm
@@ -17,41 +17,41 @@
       (+ (fib (- n 1)) (fib (- n 2)))))
 
 (define (concat a b)
-	(cond
-    	((null? a) b)
-		((null? b) a)
-    	(else 
-        	(cons (car a) (concat (cdr a) b)))))
+  (cond
+      ((null? a) b)
+    ((null? b) a)
+      (else 
+          (cons (car a) (concat (cdr a) b)))))
 
 (define (replicate x n)
-	(cond
-    	((= n 0) ())
-     	(else 
-        	(cons x (replicate x (- n 1))))))
+  (cond
+      ((= n 0) ())
+      (else 
+          (cons x (replicate x (- n 1))))))
 
 (define (uncompress s)
-	(cond 
-   		((null? s) ())
-     	(else 
-        	(concat 
-            	(replicate (car (car s)) (car (cdr (car s)))) 
-               	(uncompress (cdr s))))))
+  (cond 
+      ((null? s) ())
+      (else 
+          (concat 
+              (replicate (car (car s)) (car (cdr (car s)))) 
+                (uncompress (cdr s))))))
 
 
 (define (map fn lst)
-	(if (null? lst)
-		nil
-		(cons (fn (car lst)) (map fn (cdr lst)))))
+  (if (null? lst)
+    nil
+    (cons (fn (car lst)) (map fn (cdr lst)))))
 
 (define (deep-apply fn nested-list)
   (cond 
-  	((null? nested-list) ())
-   	((number? nested-list) (fn nested-list))
-   	((pair? (car nested-list)) 
-    	(cons (deep-apply fn (car nested-list)) 
+    ((null? nested-list) ())
+    ((number? nested-list) (fn nested-list))
+    ((pair? (car nested-list)) 
+      (cons (deep-apply fn (car nested-list)) 
               (deep-apply fn (cdr nested-list))))
-   	(else 
-    	(cons (fn (car nested-list)) 
+    (else 
+      (cons (fn (car nested-list)) 
               (deep-apply fn (cdr nested-list))))))
 
 (define (make-tree root branches) (cons root branches))
@@ -59,12 +59,12 @@
 (define (branches tree) (cdr tree))
 
 (define (reduce fn lst initial)
-	(cond 
+  (cond 
      ((null? lst) initial)
      (else (reduce fn (cdr lst) (fn initial (car lst))))))
 
 (define (tree-sum tree)
-  	(if (null? (branches tree))
+    (if (null? (branches tree))
         (root tree)
         (+ (root tree) 
            (reduce + (map tree-sum (branches tree)) 0))))
@@ -75,13 +75,13 @@
                              (make-tree 3 ()))))
 
 (define (path-product-tree t)
-	(define (helper t previous-root)
+  (define (helper t previous-root)
       (if (null? (branches t))
           (make-tree (* (root t) previous-root) ())
           (make-tree (* previous-root (root t))
-                     (map 
-                     	(lambda (b) (helper b (* previous-root (root t)))) 
-                      	(branches t)))))
+                      (map 
+                      (lambda (b) (helper b (* previous-root (root t)))) 
+  (branches t)))))
   
 
  
